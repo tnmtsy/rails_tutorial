1aed9b1b614f4b896d985de02c3655dcb1287fd1
Seiya Tanimoto <s.tanimoto@aktsk.jp>
2013-03-18 16:53:10 +0900

    Chapter8 finished.

--- depot/test/functional/products_controller_test.rb
+++ depot/test/functional/products_controller_test.rb
@@ -15,6 +15,9 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+
+    assert_select "td.list_description dl dt", 3
+    assert_select "td.list_description dl dt", "Programming Ruby 1.9"
   end
 
   test "should get new" do
